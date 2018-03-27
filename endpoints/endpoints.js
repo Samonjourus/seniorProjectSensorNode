b2de83d148004eb9baea204bdd24a4e0fde3d4c1
Benjamin Standfield <benjaminstandfield@gmail.com>
2018-03-26 20:23:36 -0400

    intial commit

---
+++ endpoints/endpoints.js
@@ -0,0 +1,9 @@
+const EXPRESS = require("express")
+
+router = EXPRESS.Router();
+
+router.get("/", function(req, res){
+  res.end(JSON.stringify{"status":"good"})
+})
+
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,32 @@
+//npm packages
+const EXPRESS = require("express")
+const FS = require("fs")
+const BODYPARSER = require("body-parser")
+
+//
+var webServer = EXPRESS();
+var port = 80;
+
+webServer.use(BODYPARSER.json())
+webServer.use(BODYPARSER.urlencoded({extended:true}))
+webServer.use(endpoints)
+
+webServer.use(function(req, res, next){
+  console.log(req.url + " " + req.method +" | "+JSON.stringify(req.body));
+  FILESYSTEM.appendFile('Server.log', " "+req.ip + " " + new Date() + " " + req.url + " " + req.method +" "+JSON.stringify(req.body) + "\"\n");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, SEARCH, DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+  if(req.method == "OPTIONS")
+    {
+      res.status(200);
+      res.end();
+    }
+  else
+    next();
+});
+
+webServer.listen(port, function(){
+  console.log("webserver running on port " + port);
+})
