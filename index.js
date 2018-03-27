7e726c11fb04ad25a42a69aa3149b643b4dadd25
Benjamin Standfield <benjaminstandfield@gmail.com>
2018-03-26 20:53:35 -0400

    Changes: * added README.md Bug Fixes: * fixed issue regarding the log file not being created properly. * fixed issue where the logging function was not being called.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //npm packages
 const EXPRESS = require("express")
-const FS = require("fs")
+const FILESYSTEM = require("fs")
 const BODYPARSER = require("body-parser")
 
 var endpoints = require("./endpoints/endpoints.js")
@@ -10,14 +10,15 @@ var port = 80;
 
 webServer.use(BODYPARSER.json())
 webServer.use(BODYPARSER.urlencoded({extended:true}))
-webServer.use(endpoints)
 
 webServer.use(function(req, res, next){
   console.log(req.url + " " + req.method +" | "+JSON.stringify(req.body));
-  FILESYSTEM.appendFile('Server.log', " "+req.ip + " " + new Date() + " " + req.url + " " + req.method +" "+JSON.stringify(req.body) + "\"\n");
+  FILESYSTEM.appendFileSync(__dirname+'\\Server.log',req.ip + " " + new Date() + " " + req.url + " " + req.method +" "+JSON.stringify(req.body) + " ||| " + JSON.stringify(req.query)+"\"\n",{flag:"a"},()=>{});
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, SEARCH, DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+webServer.use(endpoints)
 
   if(req.method == "OPTIONS")
     {
