2472e7b892b2de0c13fff2e666c1c544c8fdfd53
Benjamin Standfield <BenjaminStandfield@gmail.com>
2019-03-17 21:16:51 +0000

    updated  data logging function to split lines

--- scripts/logData.py
+++ scripts/logData.py
@@ -1,17 +1,23 @@
+
 import time
-import sys
+import serial
 
-def main():
-        index = 0
-        file = open("ok.txt", "w")
-        file.write("ok\n")
-        file.close()
-        while True:
-                print("ok")
-                sys.stdout.flush()
-                time.sleep(1)
-                index+=1
-                if(index == 5):
-                        sys.exit()
+s=serial.Serial(
+port='/dev/ttyUSB0',
+baudrate=9600,
+parity=serial.PARITY_NONE,
+stopbits=serial.STOPBITS_ONE,
+bytesize=serial.EIGHTBITS,
+timeout=1)
 
-main()
+counter=0
+while 1:
+	if(s.in_waiting):
+		x=s.read(s.in_waiting)
+		x+=s.readline()
+		x=x.decode("utf-8").split("\r\n")
+ [/#[/		for line in x:
+			if(line != ""):
+				print(line + "<<")
+	time.sleep(1)
+
